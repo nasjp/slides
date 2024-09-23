9e28609717a8226ff3fcf96baa82bc6e3fc585a6
nas <y.softvalley@hey.com>
2024-09-23 21:18:53 +0900

    ビルドプロセスに新しいindex.htmlを生成する機能を追加
    * スライドのHTMLおよびPDFファイルへのリンクを含むインデックスページを作成し、ユーザーがスライドを簡単にアクセスできるようにするため
    * 生成されたHTMLには、各スライドのタイトルとリンクが含まれ、ナビゲーションが向上する
    * .gitignoreファイルを更新し、distディレクトリの管理を改善

--- build.sh
+++ build.sh
@@ -1,6 +1,24 @@
 #!/bin/bash
 
 
+# index.htmlのヘッダーを追加
+cat <<EOT > dist/index.html
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Slides</title>
+</head>
+<body>
+    <header>
+        <h1>Slides</h1>
+    </header>
+    <main>
+        <section>
+            <ul>
+EOT
+
 # slidesディレクトリ内のすべての.mdファイルをループ処理
 for file in slides/*.md; do
   filename=$(basename "$file" .md)
@@ -14,4 +32,25 @@ for file in slides/*.md; do
   
   # HTMLの生成
   npx @marp-team/marp-cli@latest "$file" -o "$output_dir/output.html"
+  
+  # index.htmlにリンクを追加
+  echo "                <li>" >> dist/index.html
+  echo "                    <span>$filename:</span>" >> dist/index.html
+  echo "                    <ul>" >> dist/index.html
+  echo "                        <li><a href=\"${output_dir#dist/}/output.html\">HTML</a></li>" >> dist/index.html
+  echo "                        <li><a href=\"${output_dir#dist/}/output.pdf\">PDF</a></li>" >> dist/index.html
+  echo "                    </ul>" >> dist/index.html
+  echo "                </li>" >> dist/index.html
 done
+
+# index.htmlのフッターを追加
+cat <<EOT >> dist/index.html
+            </ul>
+        </section>
+    </main>
+    <footer>
+        <p>&copy; 2024 nasjp</p>
+    </footer>
+</body>
+</html>
+EOT
